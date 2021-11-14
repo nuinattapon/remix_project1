6df16e5c4c191faa07dacb90b29b1184809ac8a3
"Nattapon Sub-Anake" <"nattapon.subanake@gmail.com">
2021-11-14 12:51:38 +0700

    Update MyContract

--- contracts/MyContract.sol
+++ contracts/MyContract.sol
@@ -15,20 +15,31 @@ contract Ownable {
     }
 }
 
-contract SecretVault is Ownable{
+contract SecretVault {
     string secret;
 
     constructor(string memory _secret) {
-        super;
         secret = _secret;
     }
 
-    function getSecret() public view onlyOwner returns (string memory) {
+    function getSecret() public view returns (string memory) {
         return secret;
     }
 
 }
 
-contract MyContract {
+contract MyContract is Ownable{
+    address secretVault;
     
+    constructor(string memory _secret) {
+        super;
+        SecretVault _secretVault = new SecretVault(_secret);
+        secretVault = address(_secretVault);
+    }
+    
+    function getSecret() public view onlyOwner returns (string memory) {
+        SecretVault _secretVault = SecretVault(secretVault);
+        
+        return _secretVault.getSecret();
+    }    
 }
