42616abcaaa99f04eea9711a990784fca634dce8
"Nattapon Sub-Anake" <"nattapon.subanake@gmail.com">
2021-11-15 20:23:18 +0700

    Working FundMe.sol

---
+++ contracts/FundMe.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.4 <0.9.0;
+
+contract Fundme {
+    address payable public owner;
+
+    constructor() {
+        owner = payable(msg.sender);
+    }
+
+    mapping(address => uint256) public addressToAmountFunded;
+
+    function fund() public payable {
+        // Add requirements - check price and check room status
+        require(
+            msg.value >= 0.01 ether,
+            "Not enough Ether provided. You need to fund me at least 0.01 ETH."
+        );
+        
+        addressToAmountFunded[msg.sender] += msg.value;
+        bool sent = owner.send(msg.value);
+        require(sent, "Failed to send Ether");
+
+    }
+}
